22e081871f32fcafdc1f2e19b43e9b94910be447
Ryan Gaffney <rtgaffney@gmail.com>
2018-03-29 21:38:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 function happyHolidays()
 {  return ('Happy holidays!')}
 
-function happyHolidaysTo (name)
-{return (`Happy holidays,``${name}!`)}
+function happyHolidaysTo (name) {
+    console.log('Happy holidays,') , return '${name}!'}
